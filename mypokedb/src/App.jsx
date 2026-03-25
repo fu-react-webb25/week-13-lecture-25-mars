bf5b4d61841b89828771a62add1cc884c665a400
Santosnr6 <jesper-nyberg@hotmail.com>
2026-03-25 11:52:28 +0100

    added code

--- mypokedb/src/App.jsx
+++ mypokedb/src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import GeneratePage from "./pages/GeneratePage";
+import PokedexPage from "./pages/PokedexPage";
+import SearchPage from "./pages/SearchPage";
 
-  return (
-    <section className="app">
-      App app app...
-    </section>
-  )
+function App() {
+	return (
+		<section className="app">
+			<Header />
+
+			{/* <PokedexPage /> */}
+			{/* <GeneratePage /> */}
+			<SearchPage />
+
+			<Footer />
+		</section>
+	);
 }
 
 export default App;
---
+++ mypokedb/src/pages/SearchPage.jsx
@@ -0,0 +1,12 @@
+import SearchForm from "../components/SearchForm";
+
+const SearchPage = () => {
+	return (
+		<main className="page">
+			<h2 className="page__title">Search Pokemon</h2>
+			<SearchForm />
+		</main>
+	);
+};
+
+export default SearchPage;
